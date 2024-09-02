b98da68a8c05178f71f91824c9d27fd769ef0584
Pituxx <cristobal.andradeq5@gmail.com>
2024-09-02 16:25:13 -0400

    procesar

--- procesar.php
+++ procesar.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "digisales";
+$dbname = "digisale_wp915";
 
 // Crear conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
