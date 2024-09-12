8d4b9de8faabf9e1436de3a32c7ea3f16bbd356f
Pituxx <cristobal.andradeq5@gmail.com>
2024-09-12 10:56:11 -0300

    s

--- procesar_formulario.php
+++ procesar_formulario.php
@@ -30,7 +30,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <div style="text-align: center; padding: 20px; border-radius: 10px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); background-color: #ffffff;">
                     <img src="images/LogoDigi.png" alt="Éxito" style="width: 100px; margin-bottom: 20px;">
                     <h2 style="color: #28a745;">¡Registro Exitoso!</h2>
-                    <p style="color: #555555;">Gracias por registrarte. Hemos recibido tus datos exitosamente.</p>
+                    <p style="color: #555555;">Tu solicitud ha sido registrada! Nuestro horario de atención es desde las 8:00 hasta las 22:00 hrs. Te contactaremos a la brevedad.</p>
                     <a href="index.html" style="display: inline-block; margin-top: 20px; padding: 10px 20px; color: #ffffff; background-color: #28a745; text-decoration: none; border-radius: 5px;">Volver a la página principal</a>
                 </div>
             </div>';
